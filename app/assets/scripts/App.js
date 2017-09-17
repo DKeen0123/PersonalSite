99168d7d70208b27cf6546d68649c38b27d6e6aa
Daniel <keendaniel01@gmail.com>
2017-09-17 15:23:17 +0100

    Completed sticky header and animation of nav bar.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".about"), "60%");
+var stickyHeader = new StickyHeader();
